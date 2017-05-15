2d857a5e16b2c93fbf4ddaa2ea5a4123953047f8
Joss Mackison <joss@thinkmill.com.au>
2017-05-16 08:07:46 +1000

    tinkering with Hapi

---
+++ app.js
@@ -0,0 +1,58 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+// Inert allows the serving of static files
+server.register(require('inert'), (err) => {
+  if (err) throw err;
+
+  // Site
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      return reply.file('./public/index.html');
+    }
+  });
+});
+
+// Public Files
+server.route({
+  method: 'GET',
+  path: '/style/{file*}',
+  handler: {
+  directory: { path: 'public/style' }
+  }
+});
+server.route({
+  method: 'GET',
+  path: '/images/{file*}',
+  handler: {
+  directory: { path: 'public/images' }
+  }
+});
+
+// API
+server.route({
+  method: 'GET',
+  path: '/api/{name}',
+  handler: function (request, reply) {
+    const { headers, params } = request;
+
+    const json = {
+      "success": true,
+      "data": { headers, params }
+    };
+
+    return reply(json).code(200);
+  }
+});
+
+// Start
+server.start((err) => {
+  if (err) throw err;
+
+  console.log(`Server running at: ${server.info.uri}`);
+});
